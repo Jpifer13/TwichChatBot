4bb41ec6a3e332cb9827a239bfd8d737e4f91665
Jake Pifer <jakepifer3@gmail.com>
2021-04-15 16:14:54 -0400

    Feature/jp/roast (#2)
    * basic bot created
    
    * added roast command cause why not
    
    * Delete settings.json
    
    * gitignore
    
    * finished roast and set up where to log users

--- application/chat_bot.py
+++ application/chat_bot.py
@@ -1,12 +1,16 @@
-import irc.bot
+import os
+import random
 import requests
+from irc.bot import SingleServerIRCBot
 
 
-class TwitchBot(irc.bot.SingleServerIRCBot):
+class TwitchBot(SingleServerIRCBot):
     def __init__(self, username, client_id, token, channel):
         self.client_id = client_id
         self.token = token
-        self.channel = '#' + channel
+        self.channel = f'#{channel}'
+
+        self.line_list = self.get_insults()
 
         # Get the channel id, we will need this for v5 API calls
         url = 'https://api.twitch.tv/kraken/users?login=' + channel
@@ -18,7 +22,15 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         server = 'irc.chat.twitch.tv'
         port = 6667
         print(f'Connecting to {server} on port {str(port)}...')
-        irc.bot.SingleServerIRCBot.__init__(self, [(server, port, 'oauth:'+token)], username, username)
+        SingleServerIRCBot.__init__(self, [(server, port, 'oauth:'+token)], username, username)
+
+    def get_insults(self):
+        cwd = os.getcwd()
+        try:
+            insult_list = [line.rstrip('\n') for line in open(f'{cwd}/application/resources/insults.txt')]
+            return insult_list
+        except (FileNotFoundError, Exception) as err:
+            print(err)
 
     def on_welcome(self, c, e):
         print(f'Joining {self.channel}')
@@ -36,6 +48,9 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
             cmd = e.arguments[0].split(' ')[0][1:]
             print(f'Received command: {cmd}')
             self.do_command(e, cmd)
+        else:
+            # Log username and possibly send to db
+            pass
         return
 
     def do_command(self, e, cmd):
@@ -43,14 +58,14 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
 
         # Poll the API to get current game.
         if cmd == "game":
-            url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
+            url = f'https://api.twitch.tv/kraken/channels/{self.channel_id}'
             headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
             r = requests.get(url, headers=headers).json()
             c.privmsg(self.channel, f'{r["display_name"]} is currently playing {r["game"]}')
 
         # Poll the API the get the current status of the stream
         elif cmd == "title":
-            url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
+            url = f'https://api.twitch.tv/kraken/channels/{self.channel_id}'
             headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
             r = requests.get(url, headers=headers).json()
             c.privmsg(self.channel, f'{r["display_name"]} channel title is currently {r["status"]}')
@@ -59,8 +74,8 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         elif cmd == "raffle":
             message = "This is an example bot, replace this text with your raffle text."
             c.privmsg(self.channel, message)
-        elif cmd == "schedule":
-            message = "This is an example bot, replace this text with your schedule text."
+        elif cmd == "roast":
+            message = random.choice(self.line_list)
             c.privmsg(self.channel, message)
 
         # The command was not recognized
